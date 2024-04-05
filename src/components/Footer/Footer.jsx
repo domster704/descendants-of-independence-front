98faf7c3292f2e6bba363f869a3be48421631903
domster704 <domster704@mail.ru>
2024-04-05 16:04:26 +0300

    add header and footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as style from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <div className={style.footer}>
+            <h1>Header</h1>
+            <nav></nav>
+        </div>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as style from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={style.header}>
+            <h1>Header</h1>
+            <nav></nav>
+        </div>
+    )
+}
