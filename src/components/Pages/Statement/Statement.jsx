93489172e193b7ff123a233bc257db973b779960
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2024-04-06 20:16:19 +0600

    Added state for fields

--- src/components/Pages/Statement/Statement.jsx
+++ src/components/Pages/Statement/Statement.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styles from './Statement.module.css';
-
-import Wrapper from "../../Wrapper/Wrapper";
-import TextField from './TextField/TextField';
+import TextField from '../../UI/TextField/TextField';
+import {
+  DAY_OPTIONS,
+  EDUCATION_OPTIONS,
+  INITIAL_STATE,
+  MONTH_OPTIONS,
+  PROJECT_DIRECTION_OPTIONS,
+  REGION_OPTIONS,
+  YEAR_OPTIONS
+} from './Statement.constants';
+import chevronDown from '../../../assets/img/chevron-down.svg';
 
 const Statement = () => {
+  const [state, setState] = useState(INITIAL_STATE);
+  const [isValid, setIsValid] = useState(false);
+
+  const changeValue = (e) => {
+    const { name, value } = e.target;
+
+    if (name.includes('[')) {
+      let parts = name.split('[');
+      let firstPart = parts[0];
+      let secondPart = parts[1].slice(0, -1);
+
+      setState((prevState) => ({ ...prevState, [firstPart]: { ...prevState[firstPart], [secondPart]: value } }));
+      return;
+    }
+
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
   return (
     <div className={styles.statement}>
       <div className={styles.title}>
@@ -15,57 +41,150 @@ const Statement = () => {
       <form className={styles.form}>
         <div className={styles.text_fields}>
           <TextField
+            name="fullName"
             label="Ф.И.О"
             placeholder="Фамилия, Имя, Отчество (при наличии)"
+            value={state.fullName}
+            onChange={changeValue}
             required
           />
 
-          <div>Дата рождения</div>
+          <div className={styles.date_of_birth}>
+            <p>Дата рождения</p>
+
+            <div className={styles.date_of_birth_text_fields}>
+              <TextField
+                type="select"
+                name="dateOfBirth[day]"
+                placeholder="01"
+                options={DAY_OPTIONS}
+                currentOption={state.dateOfBirth.day}
+                onChange={changeValue}
+                required
+              />
+
+              <TextField
+                type="select"
+                name="dateOfBirth[month]"
+                placeholder="01"
+                options={MONTH_OPTIONS}
+                currentOption={state.dateOfBirth.month}
+                onChange={changeValue}
+                required
+              />
+
+              <TextField
+                type="select"
+                name="dateOfBirth[year]"
+                placeholder="2004"
+                options={YEAR_OPTIONS}
+                currentOption={state.dateOfBirth.year}
+                onChange={changeValue}
+                required
+              />
+            </div>
+          </div>
 
           <TextField
+            type="number"
+            name="number"
             label="Номер телефона"
             placeholder="моб./раб. номер телефона"
+            value={state.number}
+            onChange={changeValue}
             required
           />
 
-
           <TextField
+            type="email"
+            name="email"
             label="Эл. почта"
             placeholder="example@gmail.com"
+            value={state.email}
+            onChange={changeValue}
             required
           />
 
-          <div>Образование</div>
+          <TextField
+            type="select"
+            name="education"
+            label="Образование"
+            placeholder="Выберите образование..."
+            options={EDUCATION_OPTIONS}
+            currentOption={state.education}
+            onChange={changeValue}
+            required
+          />
 
           <TextField
+            name="academicDegree"
             label="Учёная степень, учёное звание"
+            placeholder="Не обязательно*"
+            value={state.academicDegree}
+            onChange={changeValue}
           />
 
           <div className={styles.residential_address}>
             <TextField
+              type="select"
+              name="address[region]"
               label="Адрес проживания"
-              placeholder="Алматинская область"
+              placeholder="Выберите область..."
+              options={REGION_OPTIONS}
+              currentOption={state.address.region}
+              onChange={changeValue}
               required
             />
             <TextField
+              name="address[city]"
               placeholder="Город"
+              value={state.address.city}
+              onChange={changeValue}
               required
             />
             <TextField
+              name="address[street]"
               placeholder="Улица / квартира"
+              value={state.address.street}
+              onChange={changeValue}
               required
             />
           </div>
 
           <TextField
+            name="currentActivity"
             label="Текущая деятельность"
+            placeholder="Не обязательно*"
+            value={state.currentActivity}
+            onChange={changeValue}
           />
 
-          <div>Направление проекта</div>
+          <TextField
+            type="select"
+            name="projectDirection"
+            label="Направление проекта"
+            placeholder="Выберите направление..."
+            options={PROJECT_DIRECTION_OPTIONS}
+            currentOption={state.projectDirection}
+            onChange={changeValue}
+            required
+          />
+        </div>
+
+        <div className={styles.drop_down_block_wrapper}>
+          <div className={styles.drop_down_block}>
+            <h2>Описание проекта</h2>
+            <img src={chevronDown} alt="chevron-up-img"/>
+          </div>
+
+          <div className={styles.drop_down_block}>
+            <h2>Смета расходов</h2>
+            <img src={chevronDown} alt="chevron-up-img"/>
+          </div>
         </div>
 
         <div style={{ textAlign: 'end' }}>
-          <button className={styles.submit_button}>Подать заявку</button>
+          <button disabled={!isValid} className={styles.submit_button}>Подать заявку</button>
         </div>
       </form>
     </div>
