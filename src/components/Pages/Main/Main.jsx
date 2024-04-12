8820ff7a903fd133fbc3d74af28843f17278889d
domster704 <domster704@mail.ru>
2024-04-13 00:39:10 +0300

    add margin for carth in main

--- src/components/Pages/Main/Main.jsx
+++ src/components/Pages/Main/Main.jsx
@@ -6,17 +6,17 @@ import * as style from "./Main.module.css";
 import ChatBot from "../../ChatBot/ChatBot";
 
 const Main = () => {
-  return (
-    <div className={style.main}>
-      <h1 className={style.hidden}>Main</h1>
-      <img className={style.main_logo} src={mainLogo} alt="main logo" />
-      <div style={{ position: "relative" }}>
-        <GrandInfo />
-      </div>
-      <Carth />
-      <ChatBot />
-    </div>
-  );
+    return (
+        <div className={style.main}>
+            <h1 className={style.hidden}>Main</h1>
+            <img className={style.main_logo} src={mainLogo} alt="main logo"/>
+            <div style={{position: "relative"}}>
+                <GrandInfo/>
+            </div>
+            <Carth/>
+            <ChatBot/>
+        </div>
+    );
 };
 
 export default Main;
--- src/components/Pages/Main/MainComponents/GrantInfo.jsx
+++ src/components/Pages/Main/MainComponents/GrantInfo.jsx
@@ -1,42 +1,42 @@
 import React from "react";
 import * as style from "../Main.module.css";
 import Wrapper from "../../../../components/UI/Wrapper/Wrapper";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import {useTranslation} from "react-i18next";
+import {Link} from "react-router-dom";
 
 const GrandInfo = () => {
-  const { t } = useTranslation("main");
-  return (
-    <section className={style.mainSection}>
-      <Wrapper>
-        <h2 className={style.main_title}>{t("grant_info")}</h2>
-        <div className={style.section_wrapper}>
-          <p>
-            {t("grant_info2")}
-            <Link to={"archive"}>{t("name_grant")}</Link>
-          </p>
-          <p>
-            <Link to={"archive"}>{t("gratn_to")}</Link>
-            {t("grant_info3")}
-          </p>
-          <p>
-            <Link to={"archive"}>{t("grant_year")}</Link>
-            {t("grant_info4")}
-          </p>
-          <p>
-            <Link to={"archive"}>{t("grant_year2")}</Link>
-            {t("grant_info5")}
-          </p>
-          <p className={style.main_text}>
-            {t("grant_info6")}
-            <Link to={"archive"} className={style.text_bold}>
-              {t("grant")}
-            </Link>
-          </p>
-        </div>
-      </Wrapper>
-    </section>
-  );
+    const {t} = useTranslation("main");
+    return (
+        <section className={style.mainSection}>
+            <Wrapper>
+                <h2 className={style.main_title}>{t("grant_info")}</h2>
+                <div className={style.section_wrapper}>
+                    <p>
+                        {t("grant_info2")}
+                        <Link to={"archive"}>{t("name_grant")}</Link>
+                    </p>
+                    <p>
+                        <Link to={"archive"}>{t("gratn_to")}</Link>
+                        {t("grant_info3")}
+                    </p>
+                    <p>
+                        <Link to={"archive"}>{t("grant_year")}</Link>
+                        {t("grant_info4")}
+                    </p>
+                    <p>
+                        <Link to={"archive"}>{t("grant_year2")}</Link>
+                        {t("grant_info5")}
+                    </p>
+                    <p className={style.main_text}>
+                        {t("grant_info6")}
+                        <Link to={"archive"} className={style.text_bold}>
+                            {t("grant")}
+                        </Link>
+                    </p>
+                </div>
+            </Wrapper>
+        </section>
+    );
 };
 
 export default GrandInfo;
