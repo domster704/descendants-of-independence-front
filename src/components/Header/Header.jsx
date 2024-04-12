556020f1e765793436a7959d87220f4da4650ddf
Dmitry <crazy_dima18@mail.ru>
2024-04-12 20:00:08 +0500

    fixed lang kz for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,198 +1,203 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import * as style from './Header.module.css';
-import Logo from './HeaderLogo';
-import ButtonMenu from './HeaderButtonMenu';
-import LanguageSwitcher from './HeaderLangButton';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import React, { Suspense, useEffect, useState } from "react";
+import * as style from "./Header.module.css";
+import Logo from "./HeaderLogo";
+import ButtonMenu from "./HeaderButtonMenu";
+import LanguageSwitcher from "./HeaderLangButton";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
-    const { t, i18n } = useTranslation('header');
-    const [isActive, setIsActive] = useState(false);
-    const [accordion, setAccordion] = useState(false);
-    const [mobileMenu, openMenu] = useState(false);
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-    };
+  const { t, i18n } = useTranslation("header");
+  const [isActive, setIsActive] = useState(false);
+  const [accordion, setAccordion] = useState(false);
+  const [mobileMenu, openMenu] = useState(false);
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
-    useEffect(() => {
-        const localActive = localStorage.getItem('lang');
-        if (localActive && localActive === 'kz') {
-            setIsActive(true);
-            changeLanguage(localActive);
-        } else {
-            setIsActive(false);
-            changeLanguage(localActive);
-        }
-    }, [isActive]);
+  useEffect(() => {
+    const localActive = localStorage.getItem("lang");
+    if (localActive && localActive === "kz") {
+      setIsActive(true);
+      changeLanguage(localActive);
+    } else {
+      setIsActive(false);
+      changeLanguage(localActive);
+    }
+  }, [isActive]);
 
-    const showActive = () => {
-        localStorage.setItem('lang', 'kz');
-        setIsActive(true);
-        changeLanguage('kz');
-    };
+  const showActive = () => {
+    localStorage.setItem("lang", "kz");
+    setIsActive(true);
+    changeLanguage("kz");
+  };
 
-    const closeActive = () => {
-        localStorage.setItem('lang', 'ru');
-        setIsActive(false);
-        changeLanguage('ru');
-    };
+  const closeActive = () => {
+    localStorage.setItem("lang", "ru");
+    setIsActive(false);
+    changeLanguage("ru");
+  };
 
-    const togleMore = () => {
-        setAccordion(!accordion);
-    };
+  const togleMore = () => {
+    setAccordion(!accordion);
+  };
 
-    const togleBurger = () => {
-        openMenu(!mobileMenu);
-    };
+  const togleBurger = () => {
+    openMenu(!mobileMenu);
+  };
 
-    const link = t('navigationLinks', { returnObjects: true });
-    const linkModal = t('linkModal', { returnObjects: true });
-    return (
-        <Suspense fallback="Loading...">
-            <div className={`${style.header} ${style.accordion_relative}`}>
-                <div
-                    className={`${mobileMenu ? style.mobile_bgActive : style.mobile_bg}`}
-                ></div>
-                <h1 className={style.hidden}>Header</h1>
-                <div className={style.mobile_flex}>
-                    <ButtonMenu style={style} onClick={togleBurger}/>
-                    <Logo style={style}/>
-                </div>
-                <nav className={style.navbar}>
-                    <ul className={`${style.list__type} ${style.flex}`}>
-                        {link.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    {
-                                        item.target ?
-                                            <a href={item.url} target={item.target}
-                                               rel="noopener noreferrer">{item.label}</a>
-                                            : <Link to={item.url}>{item.label}</Link>
-                                    }
-                                </li>
-                            );
-                        })}
-                        <li className={style.accordion_relative}>
-                            <button className={style.accordion__btn} onClick={togleMore}>
-                                Конкурс{' '}
-                                <span className={style.arrow}>
+  const link = t("navigationLinks", { returnObjects: true });
+  const linkModal = t("linkModal", { returnObjects: true });
+  return (
+    <Suspense fallback="Loading...">
+      <div className={`${style.header} ${style.accordion_relative}`}>
+        <div
+          className={`${mobileMenu ? style.mobile_bgActive : style.mobile_bg}`}
+        ></div>
+        <h1 className={style.hidden}>Header</h1>
+        <div className={style.mobile_flex}>
+          <ButtonMenu style={style} onClick={togleBurger} />
+          <Logo style={style} />
+        </div>
+        <nav className={style.navbar}>
+          <ul className={`${style.list__type} ${style.flex}`}>
+            {link.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item.target ? (
+                    <a
+                      href={item.url}
+                      target={item.target}
+                      rel="noopener noreferrer"
+                    >
+                      {item.label}
+                    </a>
+                  ) : (
+                    <Link to={item.url}>{item.label}</Link>
+                  )}
+                </li>
+              );
+            })}
+            <li className={style.accordion_relative}>
+              <button className={style.accordion__btn} onClick={togleMore}>
+                {t("concurse")}{" "}
+                <span className={style.arrow}>
                   <svg
-                      className={`${accordion ? '' : style.rotate}`}
+                    className={`${accordion ? "" : style.rotate}`}
+                    width="14"
+                    height="9"
+                    viewBox="0 0 14 9"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.7108 1.20986C13.6178 1.11613 13.5072 1.04174 13.3854 0.990969C13.2635 0.940201 13.1328 0.914062 13.0008 0.914062C12.8688 0.914062 12.7381 0.940201 12.6162 0.990969C12.4944 1.04174 12.3838 1.11613 12.2908 1.20986L7.71079 5.78986C7.61783 5.88359 7.50723 5.95798 7.38537 6.00875C7.26351 6.05952 7.1328 6.08566 7.00079 6.08566C6.86878 6.08566 6.73807 6.05952 6.61622 6.00875C6.49436 5.95798 6.38376 5.88359 6.29079 5.78986L1.71079 1.20986C1.61783 1.11613 1.50723 1.04174 1.38537 0.990969C1.26351 0.940201 1.1328 0.914062 1.00079 0.914062C0.868781 0.914062 0.738075 0.940201 0.616216 0.990969C0.494356 1.04174 0.383755 1.11613 0.290792 1.20986C0.104542 1.39722 0 1.65067 0 1.91486C0 2.17905 0.104542 2.4325 0.290792 2.61986L4.88079 7.20986C5.44329 7.77166 6.20579 8.08722 7.00079 8.08722C7.79579 8.08722 8.55829 7.77166 9.12079 7.20986L13.7108 2.61986C13.897 2.4325 14.0016 2.17905 14.0016 1.91486C14.0016 1.65067 13.897 1.39722 13.7108 1.20986Z"
+                      fill="black"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <ul
+                className={`${accordion ? "" : style.hidden} ${
+                  style.list__type
+                } ${style.accordion_absolute}`}
+              >
+                {linkModal.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <Link to={item.href}>{item.text}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+            <li className={style.lang__flex}>
+              <LanguageSwitcher
+                isActive={!isActive}
+                label="Рус"
+                onClick={closeActive}
+                active={style.active}
+                lang_btn={style.lang__btn}
+              />
+              <LanguageSwitcher
+                isActive={isActive}
+                label="Қаз"
+                onClick={showActive}
+                active={style.active}
+                lang_btn={style.lang__btn}
+              />
+            </li>
+          </ul>
+        </nav>
+        <div
+          className={`${style.burger_menu} ${mobileMenu ? "" : style.hidden}`}
+        >
+          <div className={style.burger__lang}>
+            <LanguageSwitcher
+              isActive={!isActive}
+              label="Рус"
+              onClick={closeActive}
+              active={style.active_lang}
+              lang_btn={style.burger_lang_btn}
+            />
+            -
+            <LanguageSwitcher
+              isActive={isActive}
+              label="Қаз"
+              onClick={showActive}
+              active={style.active_lang}
+              lang_btn={style.burger_lang_btn}
+            />
+          </div>
+          <nav className={`${style.burger_list} `}>
+            <ul className={style.list__type}>
+              {link.map((item, index) => {
+                return (
+                  <li key={index} className={style.list_item}>
+                    <Link to={item.url} className={style.concurse}>
+                      {item.label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className={style.list_item}>
+                <button className={style.concurse} onClick={togleMore}>
+                  {t("concurse")}
+                  <span>
+                    <svg
+                      className={`${accordion ? "" : style.rotate}`}
                       width="14"
                       height="9"
                       viewBox="0 0 14 9"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        d="M13.7108 1.20986C13.6178 1.11613 13.5072 1.04174 13.3854 0.990969C13.2635 0.940201 13.1328 0.914062 13.0008 0.914062C12.8688 0.914062 12.7381 0.940201 12.6162 0.990969C12.4944 1.04174 12.3838 1.11613 12.2908 1.20986L7.71079 5.78986C7.61783 5.88359 7.50723 5.95798 7.38537 6.00875C7.26351 6.05952 7.1328 6.08566 7.00079 6.08566C6.86878 6.08566 6.73807 6.05952 6.61622 6.00875C6.49436 5.95798 6.38376 5.88359 6.29079 5.78986L1.71079 1.20986C1.61783 1.11613 1.50723 1.04174 1.38537 0.990969C1.26351 0.940201 1.1328 0.914062 1.00079 0.914062C0.868781 0.914062 0.738075 0.940201 0.616216 0.990969C0.494356 1.04174 0.383755 1.11613 0.290792 1.20986C0.104542 1.39722 0 1.65067 0 1.91486C0 2.17905 0.104542 2.4325 0.290792 2.61986L4.88079 7.20986C5.44329 7.77166 6.20579 8.08722 7.00079 8.08722C7.79579 8.08722 8.55829 7.77166 9.12079 7.20986L13.7108 2.61986C13.897 2.4325 14.0016 2.17905 14.0016 1.91486C14.0016 1.65067 13.897 1.39722 13.7108 1.20986Z"
-                        fill="black"
-                    />
-                  </svg>
-                </span>
-                            </button>
-                            <ul
-                                className={`${accordion ? '' : style.hidden} ${
-                                    style.list__type
-                                } ${style.accordion_absolute}`}
-                            >
-                                {linkModal.map((item, index) => {
-                                    return (
-                                        <li key={index}>
-                                            <Link to={item.href}>{item.text}</Link>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </li>
-                        <li className={style.lang__flex}>
-                            <LanguageSwitcher
-                                isActive={!isActive}
-                                label="Рус"
-                                onClick={closeActive}
-                                active={style.active}
-                                lang_btn={style.lang__btn}
-                            />
-                            <LanguageSwitcher
-                                isActive={isActive}
-                                label="Қаз"
-                                onClick={showActive}
-                                active={style.active}
-                                lang_btn={style.lang__btn}
-                            />
-                        </li>
-                    </ul>
-                </nav>
-                <div
-                    className={`${style.burger_menu} ${mobileMenu ? '' : style.hidden}`}
-                >
-                    <div className={style.burger__lang}>
-                        <LanguageSwitcher
-                            isActive={!isActive}
-                            label="Рус"
-                            onClick={closeActive}
-                            active={style.active_lang}
-                            lang_btn={style.burger_lang_btn}
-                        />
-                        -
-                        <LanguageSwitcher
-                            isActive={isActive}
-                            label="Қаз"
-                            onClick={showActive}
-                            active={style.active_lang}
-                            lang_btn={style.burger_lang_btn}
-                        />
-                    </div>
-                    <nav className={`${style.burger_list} `}>
-                        <ul className={style.list__type}>
-                            {link.map((item, index) => {
-                                return (
-                                    <li key={index} className={style.list_item}>
-                                        <Link to={item.url} className={style.concurse}>
-                                            {item.label}
-                                        </Link>
-                                    </li>
-                                );
-                            })}
-                            <li className={style.list_item}>
-                                <button className={style.concurse} onClick={togleMore}>
-                                    Конкурс
-                                    <span>
-                    <svg
-                        className={`${accordion ? '' : style.rotate}`}
-                        width="14"
-                        height="9"
-                        viewBox="0 0 14 9"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                          d="M13.7108 1.20986C13.6178 1.11613 13.5072 1.04174 13.3854 0.990969C13.2635 0.940201 13.1328 0.914062 13.0008 0.914062C12.8688 0.914062 12.7381 0.940201 12.6162 0.990969C12.4944 1.04174 12.3838 1.11613 12.2908 1.20986L7.71079 5.78986C7.61783 5.88359 7.50723 5.95798 7.38537 6.00875C7.26351 6.05952 7.1328 6.08566 7.00079 6.08566C6.86878 6.08566 6.73807 6.05952 6.61622 6.00875C6.49436 5.95798 6.38376 5.88359 6.29079 5.78986L1.71079 1.20986C1.61783 1.11613 1.50723 1.04174 1.38537 0.990969C1.26351 0.940201 1.1328 0.914062 1.00079 0.914062C0.868781 0.914062 0.738075 0.940201 0.616216 0.990969C0.494356 1.04174 0.383755 1.11613 0.290792 1.20986C0.104542 1.39722 0 1.65067 0 1.91486C0 2.17905 0.104542 2.4325 0.290792 2.61986L4.88079 7.20986C5.44329 7.77166 6.20579 8.08722 7.00079 8.08722C7.79579 8.08722 8.55829 7.77166 9.12079 7.20986L13.7108 2.61986C13.897 2.4325 14.0016 2.17905 14.0016 1.91486C14.0016 1.65067 13.897 1.39722 13.7108 1.20986Z"
-                          fill="white"
+                        d="M13.7108 1.20986C13.6178 1.11613 13.5072 1.04174 13.3854 0.990969C13.2635 0.940201 13.1328 0.914062 13.0008 0.914062C12.8688 0.914062 12.7381 0.940201 12.6162 0.990969C12.4944 1.04174 12.3838 1.11613 12.2908 1.20986L7.71079 5.78986C7.61783 5.88359 7.50723 5.95798 7.38537 6.00875C7.26351 6.05952 7.1328 6.08566 7.00079 6.08566C6.86878 6.08566 6.73807 6.05952 6.61622 6.00875C6.49436 5.95798 6.38376 5.88359 6.29079 5.78986L1.71079 1.20986C1.61783 1.11613 1.50723 1.04174 1.38537 0.990969C1.26351 0.940201 1.1328 0.914062 1.00079 0.914062C0.868781 0.914062 0.738075 0.940201 0.616216 0.990969C0.494356 1.04174 0.383755 1.11613 0.290792 1.20986C0.104542 1.39722 0 1.65067 0 1.91486C0 2.17905 0.104542 2.4325 0.290792 2.61986L4.88079 7.20986C5.44329 7.77166 6.20579 8.08722 7.00079 8.08722C7.79579 8.08722 8.55829 7.77166 9.12079 7.20986L13.7108 2.61986C13.897 2.4325 14.0016 2.17905 14.0016 1.91486C14.0016 1.65067 13.897 1.39722 13.7108 1.20986Z"
+                        fill="white"
                       />
                     </svg>
                   </span>
-                                </button>
-                                <div
-                                    className={`${style.mobile_accordion} ${
-                                        accordion ? '' : style.hidden
-                                    }`}
-                                >
-                                    <Link to="#" className={style.concurse}>
-                                        {t('concurse_status')}
-                                    </Link>
-                                    <Link to="#" className={style.concurse}>
-                                        {t('concurse_send')}
-                                    </Link>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
+                </button>
+                <div
+                  className={`${style.mobile_accordion} ${
+                    accordion ? "" : style.hidden
+                  }`}
+                >
+                  <Link to="#" className={style.concurse}>
+                    {t("concurse_status")}
+                  </Link>
+                  <Link to="#" className={style.concurse}>
+                    {t("concurse_send")}
+                  </Link>
                 </div>
-            </div>
-        </Suspense>
-    );
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </Suspense>
+  );
 };
 
 export default Header;
