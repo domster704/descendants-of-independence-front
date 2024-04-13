9ba0f64e6183caf7a832f1e40c4e9e4b52b91ef5
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2024-04-13 13:17:00 +0600

    Added apiUrl / Fixed chatbot

--- src/components/ChatBot/ChatBot.jsx
+++ src/components/ChatBot/ChatBot.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
-import botModal from "./img/Frame 771202.png";
-import ChatModal from "./BotModal";
-import * as style from "../Pages/Main/Main.module.css";
+import React, { useState } from 'react';
+import botModal from './img/Frame 771202.png';
+import ChatModal from './BotModal';
+import * as style from '../Pages/Main/Main.module.css';
 
 const ChatBot = () => {
-  const [bot, togleBot] = useState(false);
+    const [bot, togleBot] = useState(false);
 
-  return (
-    <div className={style.bot}>
-      <button className={style.bot_bnt} onClick={() => togleBot(!bot)}>
-        <img src={botModal} alt="Bot" />
-      </button>
-      <ChatModal bot={bot} toggleBot={() => togleBot(!bot)} style={style} />
-    </div>
-  );
+    return (
+        <div className={style.bot_wrapper}>
+            <div className={style.bot}>
+                <button className={style.bot_bnt} onClick={() => togleBot(!bot)}>
+                    <img src={botModal} alt="Bot"/>
+                </button>
+                <ChatModal bot={bot} toggleBot={() => togleBot(!bot)} style={style}/>
+            </div>
+        </div>
+    );
 };
 
 export default ChatBot;
--- src/components/Pages/Main/Main.jsx
+++ src/components/Pages/Main/Main.jsx
@@ -1,20 +1,18 @@
-import React from "react";
-import Carth from "./MainComponents/Carth";
-import GrandInfo from "./MainComponents/GrantInfo";
-import mainLogo from "./img/KZ1_9633_2.webp";
-import * as style from "./Main.module.css";
-import ChatBot from "../../ChatBot/ChatBot";
+import React from 'react';
+import Carth from './MainComponents/Carth';
+import GrandInfo from './MainComponents/GrantInfo';
+import mainLogo from './img/KZ1_9633_2.webp';
+import * as style from './Main.module.css';
 
 const Main = () => {
     return (
         <div className={style.main}>
             <h1 className={style.hidden}>Main</h1>
             <img loading="lazy" className={style.main_logo} src={mainLogo} alt="main logo"/>
-            <div style={{position: "relative"}}>
+            <div style={{ position: 'relative' }}>
                 <GrandInfo/>
             </div>
             <Carth/>
-            <ChatBot/>
         </div>
     );
 };
