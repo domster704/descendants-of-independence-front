62302cbb9e03fc9042ceaa8ba0ce05e86c3e6f2f
Dmitry <crazy_dima18@mail.ru>
2024-04-14 20:09:55 +0500

    translate galery

--- src/components/Pages/Archive/Gallery/Gallery.jsx
+++ src/components/Pages/Archive/Gallery/Gallery.jsx
@@ -1,130 +1,152 @@
-import React, { useEffect } from 'react';
-import * as styles from './Gallery.module.css';
-import Wrapper from '../../../UI/Wrapper/Wrapper';
-import GalleryElement from './GalleryElement/GalleryElement';
-import ArrowButton from '../../../UI/ArrowButton/ArrowButton';
+import React, { useEffect } from "react";
+import * as styles from "./Gallery.module.css";
+import Wrapper from "../../../UI/Wrapper/Wrapper";
+import GalleryElement from "./GalleryElement/GalleryElement";
+import ArrowButton from "../../../UI/ArrowButton/ArrowButton";
 
-import gallery1 from './img/gallery1.png';
-import gallery4 from './img/gallery4.png';
-import gallery2 from './img/gallery2.png';
-import gallery3 from './img/gallery3.png';
-import * as constants from './Gallery.constants';
-import TextField from '../../../UI/TextField/TextField';
+import gallery1 from "./img/gallery1.png";
+import gallery4 from "./img/gallery4.png";
+import gallery2 from "./img/gallery2.png";
+import gallery3 from "./img/gallery3.png";
+import * as constants from "./Gallery.constants";
+import TextField from "../../../UI/TextField/TextField";
 
-import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentOption } from '../../../../store/gallerySlice';
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentOption } from "../../../../store/gallerySlice";
+import { setLanguage } from "../../../../store/gallerySlice";
 
 const Gallery = () => {
-    const { t } = useTranslation('archive');
+  const { t } = useTranslation("archive");
 
-    const dispatch = useDispatch();
-    const galleryStore = useSelector(state => state.gallery);
+  const dispatch = useDispatch();
+  const galleryStore = useSelector((state) => state.gallery);
+  const local = localStorage.getItem("lang");
+  console.log(local);
+  const galleryListRef = React.useRef(null);
+  useEffect(() => {
+    dispatch(setLanguage(local));
+  }, [t]);
+  // const [galleryList, setGalleryList] = React.useState([]);
+  //
+  // useEffect(() => {
+  //     setGalleryList([{
+  //         img: gallery1,
+  //         description: t('gallery1'),
+  //     }, {
+  //         img: gallery2,
+  //         description: t('gallery2'),
+  //     }, {
+  //         img: gallery3,
+  //         description: t('gallery3'),
+  //     }, {
+  //         img: gallery4,
+  //         description: t('gallery4'),
+  //     }, {
+  //         img: gallery4,
+  //         description: t('gallery4'),
+  //     }, {
+  //         img: gallery4,
+  //         description: t('gallery4'),
+  //     }])
+  // }, [t])
 
-    const galleryListRef = React.useRef(null);
+  const galleryOptions = useSelector((state) => state.gallery);
 
-    // const [galleryList, setGalleryList] = React.useState([]);
-    //
-    // useEffect(() => {
-    //     setGalleryList([{
-    //         img: gallery1,
-    //         description: t('gallery1'),
-    //     }, {
-    //         img: gallery2,
-    //         description: t('gallery2'),
-    //     }, {
-    //         img: gallery3,
-    //         description: t('gallery3'),
-    //     }, {
-    //         img: gallery4,
-    //         description: t('gallery4'),
-    //     }, {
-    //         img: gallery4,
-    //         description: t('gallery4'),
-    //     }, {
-    //         img: gallery4,
-    //         description: t('gallery4'),
-    //     }])
-    // }, [t])
+  const dropdownTextField = (
+    <TextField
+      type="select"
+      name="gallery"
+      options={galleryOptions.dropdownOptions}
+      currentOption={galleryOptions.currentOption}
+      onChange={(e) => dispatch(setCurrentOption(e.target.value))}
+    />
+  );
 
-    const galleryOptions = useSelector(state => state.gallery);
+  console.log();
 
-    const dropdownTextField = (
-        <TextField type="select"
-                   name="gallery"
-                   options={galleryOptions.dropdownOptions}
-                   currentOption={galleryOptions.currentOption}
-                   onChange={(e) => dispatch(setCurrentOption(e.target.value))}
-        />
-    );
-
-    console.log()
-
-    return (
-        <div className={styles.gallery}>
-            <Wrapper>
-                <div className={styles.dropdown_list_mobile}>{dropdownTextField}</div>
-                <div className={styles.header_mobile}>
-                    <h1>{t('gallery')}</h1>
-                    <div className={styles.gallery_list_arrows}>
-                        <ArrowButton size="large"
-                                     onClick={() => {
-                                         galleryListRef.current.scroll({
-                                             left: galleryListRef.current.scrollLeft - constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
-                                             behavior: 'smooth',
-                                         });
-                                     }}/>
-                        <ArrowButton size="large"
-                                     direction="right"
-                                     onClick={() => {
-                                         galleryListRef.current.scroll({
-                                             left: galleryListRef.current.scrollLeft + constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
-                                             behavior: 'smooth',
-                                         });
-                                     }}/>
-                    </div>
-                </div>
-                <div className={styles.dropdown_list_desktop}>{dropdownTextField}</div>
-                <div className={styles.gallery_list_block}>
-                    <div ref={galleryListRef} className={styles.gallery_list}>
-                        {
-                            galleryStore.galleryList.map((item, index) => {
-                                if (galleryOptions.currentOption.value !== 'all' && galleryOptions.currentOption.value !== item.type) {
-                                    return null;
-                                }
-                                return (
-                                    <GalleryElement
-                                        key={index}
-                                        type={item.type}
-                                        img={item.img}
-                                        description={item.description}
-                                        link={item?.link}
-                                    />
-                                )
-                            })
-                        }
-                    </div>
-                    <div className={styles.gallery_list_arrows}>
-                        <ArrowButton size="large"
-                                     onClick={() => {
-                                         galleryListRef.current.scroll({
-                                             left: galleryListRef.current.scrollLeft - constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
-                                             behavior: 'smooth',
-                                         });
-                                     }}/>
-                        <ArrowButton size="large"
-                                     direction="right"
-                                     onClick={() => {
-                                         galleryListRef.current.scroll({
-                                             left: galleryListRef.current.scrollLeft + constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
-                                             behavior: 'smooth',
-                                         });
-                                     }}/>
-                    </div>
-                </div>
-            </Wrapper>
+  return (
+    <div className={styles.gallery}>
+      <Wrapper>
+        <div className={styles.dropdown_list_mobile}>{dropdownTextField}</div>
+        <div className={styles.header_mobile}>
+          <h1>{t("gallery")}</h1>
+          <div className={styles.gallery_list_arrows}>
+            <ArrowButton
+              size="large"
+              onClick={() => {
+                galleryListRef.current.scroll({
+                  left:
+                    galleryListRef.current.scrollLeft -
+                    constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
+                  behavior: "smooth",
+                });
+              }}
+            />
+            <ArrowButton
+              size="large"
+              direction="right"
+              onClick={() => {
+                galleryListRef.current.scroll({
+                  left:
+                    galleryListRef.current.scrollLeft +
+                    constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
+                  behavior: "smooth",
+                });
+              }}
+            />
+          </div>
         </div>
-    );
-}
+        <div className={styles.dropdown_list_desktop}>{dropdownTextField}</div>
+        <div className={styles.gallery_list_block}>
+          <div ref={galleryListRef} className={styles.gallery_list}>
+            {galleryStore.galleryList.map((item, index) => {
+              if (
+                galleryOptions.currentOption.value !== "all" &&
+                galleryOptions.currentOption.value !== item.type
+              ) {
+                return null;
+              }
+              return (
+                <GalleryElement
+                  key={index}
+                  type={item.type}
+                  img={item.img}
+                  description={item.description}
+                  link={item?.link}
+                />
+              );
+            })}
+          </div>
+          <div className={styles.gallery_list_arrows}>
+            <ArrowButton
+              size="large"
+              onClick={() => {
+                galleryListRef.current.scroll({
+                  left:
+                    galleryListRef.current.scrollLeft -
+                    constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
+                  behavior: "smooth",
+                });
+              }}
+            />
+            <ArrowButton
+              size="large"
+              direction="right"
+              onClick={() => {
+                galleryListRef.current.scroll({
+                  left:
+                    galleryListRef.current.scrollLeft +
+                    constants.GALLERY_ELEMENT_WITH_GAP_WIDTH_PX,
+                  behavior: "smooth",
+                });
+              }}
+            />
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
 
 export default Gallery;
--- src/store/gallerySlice.js
+++ src/store/gallerySlice.js
@@ -1,73 +1,174 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-import winner_2021_awarding_1 from '../assets/img/gallery/winner_2021_awarding_1.webp';
-import winner_2021_awarding_2 from '../assets/img/gallery/winner_2021_awarding_2.webp';
-import winner_2021_awarding_3 from '../assets/img/gallery/winner_2021_awarding_3.webp';
-import winner_2021_awarding_4 from '../assets/img/gallery/winner_2021_awarding_4.webp';
-import winner_2021_awarding_5 from '../assets/img/gallery/winner_2021_awarding_5.webp';
-import winner_2021_awarding_6 from '../assets/img/gallery/winner_2021_awarding_6.webp';
-import news_1 from '../assets/img/gallery/news_1.webp';
-import news_2 from '../assets/img/gallery/news_2.webp';
-import news_3 from '../assets/img/gallery/news_3.webp';
+import winner_2021_awarding_1 from "../assets/img/gallery/winner_2021_awarding_1.webp";
+import winner_2021_awarding_2 from "../assets/img/gallery/winner_2021_awarding_2.webp";
+import winner_2021_awarding_3 from "../assets/img/gallery/winner_2021_awarding_3.webp";
+import winner_2021_awarding_4 from "../assets/img/gallery/winner_2021_awarding_4.webp";
+import winner_2021_awarding_5 from "../assets/img/gallery/winner_2021_awarding_5.webp";
+import winner_2021_awarding_6 from "../assets/img/gallery/winner_2021_awarding_6.webp";
+import news_1 from "../assets/img/gallery/news_1.webp";
+import news_2 from "../assets/img/gallery/news_2.webp";
+import news_3 from "../assets/img/gallery/news_3.webp";
 
+import i18n from "../i18next";
+
+const lng = localStorage.getItem("lang");
+let getArchiveTranslationRu = (key, lng) => {
+  return i18n.t(`archive:${key}`, { lng: `${lng}` });
+};
 const initialState = {
-    dropdownOptions: [
-        { value: 'news', label: 'Новости' },
-        { value: 'photo', label: 'Фотоотчет' },
-        { value: 'all', label: 'Всё' },
-    ],
-    currentOption: { value: 'all', label: 'Всё' },
-    galleryList: [{
-        type: 'photo',
-        description: 'Награждение победителей гранта 2021 года',
-        img: winner_2021_awarding_1
-    }, {
-        type: 'photo',
-        description: 'Награждение победителей гранта 2021 года',
-        img: winner_2021_awarding_2
-    }, {
-        type: 'photo',
-        description: 'Награждение победителей гранта 2021 года',
-        img: winner_2021_awarding_3
-    }, {
-        type: 'photo',
-        description: 'Награждение победителей гранта 2021 года',
-        img: winner_2021_awarding_4
-    }, {
-        type: 'photo',
-        description: 'Награждение победителей гранта 2021 года',
-        img: winner_2021_awarding_5
-    }, {
-        type: 'photo',
-        description: 'Награждение победителей гранта 2021 года',
-        img: winner_2021_awarding_6
-    }, {
-        type: 'news',
-        description: '3 миллион теңгеден 30 грант беріледі',
-        img: news_1,
-        link: 'https://egemen.kz/article/282168-tauelsizdik-urpaqtary-3-million-tenhgeden-30-grant-beriledi'
-    }, {
-        type: 'news',
-        description: 'Глава государства принял участие в церемонии вручения грантов «Тәуелсіздік ұрпақтары»',
-        img: news_2,
-        link: 'https://akorda.kz/ru/glava-gosudarstva-prinyal-uchastie-v-ceremonii-vrucheniya-grantov-tauelsizdik-urpaktary-8113634 '
-    }, {
-        type: 'news',
-        description: '33 проекта стали победителями гранта «Тәуелсіздік ұрпақтары»',
-        img: news_3,
-        link: 'https://el.kz/ru/33-proekta-stali-pobeditelyami-granta-tauelsizdik-urpaqtary_58700/'
-    }]
-}
+  lang: "ru",
+  dropdownOptions: [
+    {
+      value: "news",
+      label: getArchiveTranslationRu("news", "ru"),
+    },
+    { value: "photo", label: getArchiveTranslationRu("foto", "ru") },
+    { value: "all", label: getArchiveTranslationRu("all", "ru") },
+  ],
+  currentOption: { value: "all", label: getArchiveTranslationRu("all", "ru") },
+  galleryList: [
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", "ru"),
+      img: winner_2021_awarding_1,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", "ru"),
+      img: winner_2021_awarding_2,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", "ru"),
+      img: winner_2021_awarding_3,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", "ru"),
+      img: winner_2021_awarding_4,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", "ru"),
+      img: winner_2021_awarding_5,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", "ru"),
+      img: winner_2021_awarding_6,
+    },
+    {
+      type: "news",
+      description: getArchiveTranslationRu("30", "ru"),
+      img: news_1,
+      link: "https://egemen.kz/article/282168-tauelsizdik-urpaqtary-3-million-tenhgeden-30-grant-beriledi",
+    },
+    {
+      type: "news",
+      description: getArchiveTranslationRu("bastik", "ru"),
+      img: news_2,
+      link: "https://akorda.kz/ru/glava-gosudarstva-prinyal-uchastie-v-ceremonii-vrucheniya-grantov-tauelsizdik-urpaktary-8113634 ",
+    },
+    {
+      type: "news",
+      description: getArchiveTranslationRu("33", "ru"),
+      img: news_3,
+      link: "https://el.kz/ru/33-proekta-stali-pobeditelyami-granta-tauelsizdik-urpaqtary_58700/",
+    },
+  ],
+};
+
+const updateDropdownOptions = (lang) => [
+  {
+    value: "news",
+    label: getArchiveTranslationRu("news", lang),
+  },
+  {
+    value: "photo",
+    label: getArchiveTranslationRu("foto", lang),
+  },
+  {
+    value: "all",
+    label: getArchiveTranslationRu("all", lang),
+  },
+];
+
+const updateCurrentOptionLabel = (lang) => {
+  return {
+    value: "all",
+    label: getArchiveTranslationRu("all", lang),
+  };
+};
+
+const updateGalleryList = (lang) => {
+  return [
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", lang),
+      img: winner_2021_awarding_1,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", lang),
+      img: winner_2021_awarding_2,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", lang),
+      img: winner_2021_awarding_3,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", lang),
+      img: winner_2021_awarding_4,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", lang),
+      img: winner_2021_awarding_5,
+    },
+    {
+      type: "photo",
+      description: getArchiveTranslationRu("2021", lang),
+      img: winner_2021_awarding_6,
+    },
+    {
+      type: "news",
+      description: getArchiveTranslationRu("30", lang),
+      img: news_1,
+      link: "https://egemen.kz/article/282168-tauelsizdik-urpaqtary-3-million-tenhgeden-30-grant-beriledi",
+    },
+    {
+      type: "news",
+      description: getArchiveTranslationRu("bastik", lang),
+      img: news_2,
+      link: "https://akorda.kz/ru/glava-gosudarstva-prinyal-uchastie-v-ceremonii-vrucheniya-grantov-tauelsizdik-urpaktary-8113634 ",
+    },
+    {
+      type: "news",
+      description: getArchiveTranslationRu("33", lang),
+      img: news_3,
+      link: "https://el.kz/ru/33-proekta-stali-pobeditelyami-granta-tauelsizdik-urpaqtary_58700/",
+    },
+  ];
+};
 
 const gallerySlice = createSlice({
-    name: 'gallery',
-    initialState,
-    reducers: {
-        setCurrentOption: (state, { payload }) => {
-            state.currentOption = payload;
-        }
-    }
+  name: "gallery",
+  initialState,
+  reducers: {
+    setCurrentOption: (state, { payload }) => {
+      state.currentOption = payload;
+    },
+    setLanguage: (state, { payload }) => {
+      state.lang = payload;
+      state.dropdownOptions = updateDropdownOptions(payload);
+      state.currentOption = updateCurrentOptionLabel(payload);
+      state.galleryList = updateGalleryList(payload);
+    },
+  },
 });
 
 export default gallerySlice.reducer;
-export const { setCurrentOption } = gallerySlice.actions;
+export const { setCurrentOption, setLanguage } = gallerySlice.actions;
