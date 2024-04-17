4cee5ec66d6999965ec9e00a96f5d725c6a1bf63
Dmitry <crazy_dima18@mail.ru>
2024-04-17 23:39:05 +0500

    fixed header for mobile and fix title translate for statement

--- src/components/Pages/Statement/Statement.jsx
+++ src/components/Pages/Statement/Statement.jsx
@@ -1,25 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { setIsFormError } from '../../../store/envSlice';
-import { COST_ESTIMATE_INITIAL_STATE, INITIAL_STATE, PROJECT_DESCRIPTION_INITIAL_STATE } from './Statement.constants';
-import StatementDropdownBlock from './components/StatementDropdownBlock/StatementDropdownBlock';
-import StatementDropzone from './components/StatementDropzone/StatementDropzone';
-import StatementMainFields from './components/StatementMainFields/StatementMainFields';
-import * as styles from './Statement.module.css';
-import { useTranslation } from 'react-i18next';
-import { createApplication, fetchCategories } from '../../../store/applicationThunk';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setIsFormError } from "../../../store/envSlice";
+import {
+    COST_ESTIMATE_INITIAL_STATE,
+    INITIAL_STATE,
+    PROJECT_DESCRIPTION_INITIAL_STATE,
+} from "./Statement.constants";
+import StatementDropdownBlock from "./components/StatementDropdownBlock/StatementDropdownBlock";
+import StatementDropzone from "./components/StatementDropzone/StatementDropzone";
+import StatementMainFields from "./components/StatementMainFields/StatementMainFields";
+import * as styles from "./Statement.module.css";
+import { useTranslation } from "react-i18next";
+import {
+    createApplication,
+    fetchCategories,
+} from "../../../store/applicationThunk";
 
 const Statement = () => {
-    const { t } = useTranslation('statement');
+    const { t } = useTranslation("statement");
 
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
 
     const [state, setState] = useState(INITIAL_STATE);
-    const [projectDescription, setProjectDescription] = useState(PROJECT_DESCRIPTION_INITIAL_STATE);
-    const [costEstimate, setCostEstimate] = useState(COST_ESTIMATE_INITIAL_STATE);
+    const [projectDescription, setProjectDescription] = useState(
+        PROJECT_DESCRIPTION_INITIAL_STATE
+    );
+    const [costEstimate, setCostEstimate] = useState(
+        COST_ESTIMATE_INITIAL_STATE
+    );
 
     useEffect(() => {
         dispatch(fetchCategories());
@@ -32,14 +43,24 @@ const Statement = () => {
     const changeValue = (e) => {
         const { name, value } = e.target;
 
-        if (name === 'phone') {
-            if (value.length && (value !== '+' && isNaN(Number(value)) || Number(value) < 1)) return;
+        if (name === "phone") {
+            if (
+                value.length &&
+                ((value !== "+" && isNaN(Number(value))) || Number(value) < 1)
+            )
+                return;
             setState((prevState) => ({ ...prevState, [name]: value.trim() }));
             return;
         }
 
-        if (projectDescription !== null && Object.keys(projectDescription).includes(name)) {
-            setProjectDescription((prevState) => ({ ...prevState, [name]: value }));
+        if (
+            projectDescription !== null &&
+            Object.keys(projectDescription).includes(name)
+        ) {
+            setProjectDescription((prevState) => ({
+                ...prevState,
+                [name]: value,
+            }));
             return;
         }
 
@@ -51,8 +72,10 @@ const Statement = () => {
 
         const isStateValid = (obj) => {
             for (const key in obj) {
-                if (key === 'cost_estimate') {
-                    return !Boolean(obj[key].find(item => !isStateValid(item)));
+                if (key === "cost_estimate") {
+                    return !Boolean(
+                        obj[key].find((item) => !isStateValid(item))
+                    );
                 }
 
                 if (Array.isArray(obj[key])) {
@@ -60,13 +83,22 @@ const Statement = () => {
                     else continue;
                 }
 
-                if (typeof obj[key] === 'object' && obj[key] !== null && !isStateValid(obj[key])) return false;
-                if (obj[key] === '') return false;
+                if (
+                    typeof obj[key] === "object" &&
+                    obj[key] !== null &&
+                    !isStateValid(obj[key])
+                )
+                    return false;
+                if (obj[key] === "") return false;
             }
             return true;
         };
 
-        const resultState = { ...state, ...projectDescription, cost_estimate: costEstimate };
+        const resultState = {
+            ...state,
+            ...projectDescription,
+            cost_estimate: costEstimate,
+        };
 
         if (!isStateValid(resultState)) {
             dispatch(setIsFormError(true));
@@ -78,22 +110,20 @@ const Statement = () => {
         } catch (e) {
             // Error code...
         } finally {
-            navigate('/success');
+            navigate("/success");
         }
     };
 
     return (
         <div className={styles.statement}>
             <div className={styles.title}>
-                <h1>{t('title')}</h1>
-                <p>{t('title_description')}</p>
+                <p style={{ paddingBottom: "5px" }}>{t("cust_title")}</p>
+                <h1>{t("title")}</h1>
+                <p>{t("title_description")}</p>
             </div>
 
             <form onSubmit={sendData} className={styles.form}>
-                <StatementMainFields
-                    state={state}
-                    changeValue={changeValue}
-                />
+                <StatementMainFields state={state} changeValue={changeValue} />
 
                 <StatementDropdownBlock
                     projectDescription={projectDescription}
@@ -103,12 +133,11 @@ const Statement = () => {
                     setCostEstimate={setCostEstimate}
                 />
 
-                <StatementDropzone
-                    state={state}
-                    setState={setState}
-                />
+                <StatementDropzone state={state} setState={setState} />
 
-                <button className={styles.submit_button}>{t('submit_button')}</button>
+                <button className={styles.submit_button}>
+                    {t("submit_button")}
+                </button>
             </form>
         </div>
     );
