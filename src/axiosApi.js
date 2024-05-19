27a96be11e45b4acf12b6ea7a6676ee4068f3e41
domster704 <domster704@mail.ru>
2024-05-19 20:24:31 +0300

    add cors

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -3,6 +3,9 @@ import { apiUrl } from './constants';
 
 const axiosApi = axios.create({
     baseURL: apiUrl,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    }
 });
 
 export default axiosApi;
